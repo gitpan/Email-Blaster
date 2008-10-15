ca1f6ff52f4dd16b5736ae1100890c17e1dff136
John Drago <jdrago_999@yahoo.com>
2008-10-15 23:13:59 +0000

    Import of JOHND/Email-Blaster-0.0001_01 from CPAN.
    gitpan-cpan-distribution: Email-Blaster
    gitpan-cpan-version:      0.0001_01
    gitpan-cpan-path:         JOHND/Email-Blaster-0.0001_01.tar.gz
    gitpan-cpan-author:       JOHND
    gitpan-cpan-maturity:     developer

---
+++ lib/Email/Blaster.pm
@@ -0,0 +1,36 @@
+package Email::Blaster;
+
+use strict;
+use warnings 'all';
+
+our $VERSION = '0.0001_01';
+
+__END__
+
+=pod
+
+=head1 NAME
+
+Email::Blaster - Scalable Mass Email System
+
+=head1 SYNOPSIS
+
+TBD
+
+=head1 DESCRIPTION
+
+TBD
+
+=head1 AUTHOR
+
+John Drago <jdrago_999@yahoo.com>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2008 by John Drago
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself, either Perl version 5.10.0 or,
+at your option, any later version of Perl 5 you may have available.
+
+=cut
